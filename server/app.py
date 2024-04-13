ddbe19f74e5ca62687eff3416e8b8f8f93c716d7
Immanuel <immanuelcherui1@gmail.com>
2024-04-13 22:13:04 +0300

    app skeleton

---
+++ server/app.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python3
+
+from flask import Flask, request, make_response, jsonify
+from flask_migrate import Migrate
+
+from models import *
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.json.compact = False
+
+migrate = Migrate(app, db)
+
+db.init_app(app)
+
+@app.route('/')
+def home():
+    return ''
+
+@app.route('/restaurants')
+def restaurants():
+    return ''
+
+@app.route('/restaurants/<int:id>', methods= ['GET', 'DELETE'])
+def restaurants_by_id(id):
+    return ''
+
+@app.route('/pizzas')
+def pizzas():
+    return ''
+
+@app.route('/restaurant_pizzas', methods=['POST'])
+def restaurant_pizzas():
+    return ''
+
+
+if __name__ == '__main__':
+    app.run(port=5555, debug=True)
